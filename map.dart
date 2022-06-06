2c590cf1409a1848921aa4400acd677427f65dd9
vishal <mahatovishal2001@email.com>
2022-06-06 08:40:04 +0530

    class and object

--- map.dart
+++ map.dart
@@ -1,12 +1,41 @@
+import 'dart:io';
+
+
+class Student {
+  final String? title;
+  final int? rollNo;
+  final int? noOfSub;
+  final Map<String,int>? subjects;
+
+  Student({this.title, this.rollNo, this.noOfSub, this.subjects});
+}
+
+
 void main() 
 {
-Map<String,List<int>>student={
-
-};
-Map<String,Map<String,int>> studentMarks={};
-studentMarks['vishal']={'computer':90,'chemistry':70,'physics':60};
-studentMarks['subhojeet']={'chemistry':91,'ccomputer':95,'physics':67};
-studentMarks['ananya']={'chemistry':76,'computer':89,'physics':75};
-print(studentMarks);
-
+  Map<String ,Student> studentdetail={};
+print('enter no of student',);
+int? n=int.parse(stdin.readLineSync()!);
+for(int i=0;i<n;i++)
+{
+  
+  print('enter name of student $i ');
+  String title=stdin.readLineSync()!;
+  print('enter no of subjects');
+  int noOfSub=int.parse(stdin.readLineSync()!);
+  print('enter rollno');
+  int rollNo=int.parse(stdin.readLineSync()!);
+  Map<String,int>subject={};
+  for(int j=0;j<noOfSub;j++)
+  {
+    print('enter subject name');
+      String subjectname= stdin.readLineSync()!;
+      print('enter marks');
+      int marks=int.parse(stdin.readLineSync()!);
+      subject[subjectname]=marks;
+  }
+  Student s=Student(subjects: subject,noOfSub: noOfSub,rollNo: rollNo,title: title);
+  studentdetail[title]=s;
+  }
+  print(studentdetail.entries);
 }
